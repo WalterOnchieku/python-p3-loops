ad760c53118a373bce8d2be1a34f1d0a6df7e886
root <root@DESKTOP-RHA5CJR.localdomain>
2024-08-29 16:19:02 +0300

    conditional statements

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i>0:
+        print(i)
+        i-=1
+    print('Happy New Year!')    
+            
 
-def square_integers(int_list):
-    # code goes here!
-    pass
+   
+
+def square_integers(int_list=[1, 2, 3, 4, 5]):
+    int_list2=[]
+    for i in int_list:
+        int_list2.append(i**2)
+        print (int_list2)
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
